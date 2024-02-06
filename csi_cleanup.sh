f554cc20ea535c8c7994500003c37db628061f34
7069wrk <102612356+7069wrk@users.noreply.github.com>
2024-02-05 22:05:13 -0600

    Update csi_cleanup.sh
    remove powerup. leave only the cleanup features

--- csi_cleanup.sh
+++ csi_cleanup.sh
@@ -73,31 +73,3 @@ if dpkg --print-foreign-architectures | grep -q 'i386'; then
     echo $key | sudo -S dpkg --remove-architecture i386
 fi
 echo $key | sudo -S apt autoremove 
-Then run the powerup code:
-
-#!/bin/bash
-clear
-timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-output_file="/tmp/powerup-$timestamp.txt"
-
-while true; do
-    key=$(zenity --password --title "Power up your system with an upgrade." --text "Enter your CSI password." --width=400)
-    if [ $? -ne 0 ]; then
-        zenity --info --text="Operation cancelled. Exiting script." --width=400
-        exit 1
-    fi
-    if echo "$key" | sudo -S -k -l &> /dev/null; then
-        break # Exit loop if the password is correct
-    else
-        zenity --error --title="Authentication Failure" --text="Incorrect password or lack of sudo privileges. Please try again." --width=400
-    fi
-done
-
-cd /tmp
-SCRIPT_URL="https://raw.githubusercontent.com/CSILinux/CSILinux-Powerup/main/csitoolsupdate.sh"
-curl -o csitoolsupdate.sh "$SCRIPT_URL" && chmod +x csitoolsupdate.sh
-echo "Running update now..." | tee "$output_file"
-
-
-
-./csitoolsupdate.sh $key | tee -a "$output_file"
